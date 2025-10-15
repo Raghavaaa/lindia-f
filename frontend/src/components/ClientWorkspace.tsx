117aa58b8da8e22ac0b421b9c35e4229cddf4167
Raghavan Karthik <raghavankarthik@MacBookPro.lan>
2025-10-16 02:59:21 +0530

    feat: Move module pills more toward center and higher
    - Increase left movement from -60px to -120px (move more toward center)
    - Increase upward movement from -20px to -40px (move higher)
    - Module pills now positioned more centrally and elevated

--- frontend/src/components/ClientWorkspace.tsx
+++ frontend/src/components/ClientWorkspace.tsx
@@ -128,7 +128,7 @@ export default function ClientWorkspace({
         </div>
 
         {/* Right side - Module pills */}
-        <div style={{ width: 280, flexShrink: 0, marginTop: -20, marginLeft: -60 }}>
+        <div style={{ width: 280, flexShrink: 0, marginTop: -40, marginLeft: -120 }}>
           <ModulePills activeModule={activeModule} onSelect={onModuleChange} />
         </div>
       </div>
