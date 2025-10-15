af5a0b80486fd9a2740926a3dc1f83e935533cb3
Raghavan Karthik <raghavankarthik@MacBookPro.lan>
2025-10-16 03:04:39 +0530

    feat: Move module pills further to center
    - Increase marginLeft from -120px to -180px for more centered position

--- frontend/src/components/ClientWorkspace.tsx
+++ frontend/src/components/ClientWorkspace.tsx
@@ -128,7 +128,7 @@ export default function ClientWorkspace({
         </div>
 
         {/* Right side - Module pills */}
-        <div style={{ width: 320, flexShrink: 0, marginTop: -60, marginLeft: -120 }}>
+        <div style={{ width: 320, flexShrink: 0, marginTop: -60, marginLeft: -180 }}>
           <ModulePills activeModule={activeModule} onSelect={onModuleChange} />
         </div>
       </div>
